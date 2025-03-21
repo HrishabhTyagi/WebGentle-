028461a63685571661e06ccb219684e8f8351881
HrishabHTyagi <95836061+HrishabhTyagi@users.noreply.github.com>
2025-03-21 10:38:26 +0530

    Add project files.

---
+++ WebGentle.Api/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using WebGentle.Application
+
+using WebGentle.Infrastructure;
+
+namespace WebGentle.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+            return services;
+        }
+    }
+}
---
+++ WebGentle.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WebGentle.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDI(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
